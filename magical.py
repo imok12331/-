60b71fa6f657235d64189e341802d7e1c4da125f
imok12331 <49380877+imok12331@users.noreply.github.com>
2020-07-07 10:34:10 +0800

    Update magical.py

--- magical.py
+++ magical.py
@@ -6,7 +6,7 @@ M = datetime.datetime.now().month
 D = datetime.datetime.now().day
 if M == 2:
     if D == 14:
-        print('\n\n♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥\n♥节日快乐♥永远爱你♥\n♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥\n\n')
+        print('\n\n♥\n\n')
 if D == 1:
     aims = float(input('请更新月目标'))
     sche = 0 #月达成归零
